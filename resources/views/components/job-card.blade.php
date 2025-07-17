02986bd7f5d64f398bd21e372efd0be6b7a93d71
GRASiN-BooP <gaurav.singh@gocomet.com>
2025-07-17 16:02:38 +0530

    setup complete created nav and featured job section

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,18 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">GRASiN</div>
+
+    <div class="py-8 font-bold">
+        <h4>Editor</h4>
+        <p>Full Time - From $60, 000</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div class="">
+            <x-tags>Tags</x-tags>
+            <x-tags>Tags</x-tags>
+            <x-tags>Tags</x-tags>
+        </div>
+
+        <img src="https://placehold.co/42x42" alt="" class="rounded-xl">
+    </div>
+</div>
